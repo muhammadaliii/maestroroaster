93fe49365b708f9b7502f24586128f0067598aa4
Muhammad Ali <muhammadali.alaydrus@icloud.com>
2021-01-12 00:43:19 +0700

    update content

--- src/js/components/filter.js
+++
@@ -1,9 +0,0 @@
-(function($){
-
-    if ($('.portofolio .portofolio-area').length > 0) {
-		var containerEl = document.querySelector('.portofolio .portofolio-area');
-		var mixer = mixitup(containerEl);
-    }
-
-
-})(jQuery);
